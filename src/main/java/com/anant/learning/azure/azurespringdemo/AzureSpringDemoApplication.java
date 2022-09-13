756a81a1d5fe4aa9e06b457c3f31209b6136c87d
Anant Tripathi <ianantktripathi@gmail.com>
2022-09-13 15:32:44 +0530

    first commit

---
+++ src/main/java/com/anant/learning/azure/azurespringdemo/AzureSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.anant.learning.azure.azurespringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anant/learning/azure/azurespringdemo/AzureSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anant.learning.azure.azurespringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
