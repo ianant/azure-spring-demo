0604fa966db5b3b6f870d3087dd427be4570e3f9
Anant Tripathi <ianantktripathi@gmail.com>
2022-09-13 21:19:50 +0530

    second commit

--- src/main/java/com/anant/learning/azure/azurespringdemo/AzureSpringDemoApplication.java
+++ src/main/java/com/anant/learning/azure/azurespringdemo/AzureSpringDemoApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class AzureSpringDemoApplication {
+
+	@GetMapping("/message")
+	public String getMessage(){
+		return "deploy your application on Azure:"
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureSpringDemoApplication.class, args);
